97be3164ac59084a6cf85c86797bf7148d5201fd
jiakang1998 <55350536+jiakang1998@users.noreply.github.com>
2021-02-09 14:53:55 +1100

    xxx

---
+++ app/src/App.vue
@@ -0,0 +1,304 @@
+<template>
+    <div id="app">
+        <StyleEditor ref="styleEditor" :code="currentStyle"></StyleEditor>
+        <ResumeEditor ref="resumeEditor" :markdown="currentMarkdown" :enableHtml="enableHtml"></ResumeEditor>
+    </div>
+</template>
+<script>
+import StyleEditor from './components/StyleEditor'
+import ResumeEditor from './components/ResumeEditor'
+import './assets/reset.css'
+let isPc = (function() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone",
+        "SymbianOS", "Windows Phone",
+        "iPad", "iPod"
+    ];
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}());
+let getDateDiff = function(startDate, endDate) {
+    var startTime = new Date(Date.parse(startDate.replace(/-/g, "/"))).getTime();
+    var endTime = new Date(Date.parse(endDate.replace(/-/g, "/"))).getTime();
+    var dates = Math.abs((startTime - endTime)) / (1000 * 60 * 60 * 24);
+    return dates;
+}
+document.title += getDateDiff((new Date()).getFullYear() + '-' + ((new Date()).getMonth() + 1) + '-' + (new Date()).getDate(), '2016-09-15') + 1 + '天';
+export default {
+    name: 'app',
+    components: {
+        StyleEditor,
+        ResumeEditor
+    },
+    data() {
+        return {
+            interval: 27,
+            currentStyle: '',
+            enableHtml: false,
+            fullStyle: [
+                `/*
+* Hi, 我的宝贝李欣航 我们在一起都快一年了！
+* 马上就要到2.14了 这是我们的第一个情人节 嘿嘿
+* 每次思考礼物的时候我都很困扰，所以这次我选择用这样的方式来表达我们这一年的回忆
+* 你老是说我是修电脑的，但我真的不会修电脑啊！但我会写网页
+* 比如这个网页。就是个什么也没有的网页。
+* 我学的东西就是如何让这个网页变得更美 更能表达我的心意
+* 比如说 我知道你是在用手机看还是电脑看嘿嘿
+* 滴滴 监测到航宝使用的设备是： ${isPc ? '电脑' : '手机'}
+*/
+
+/* 首先给所有元素加上过渡效果 */
+* {
+  -webkit-transition: all .3s;
+  transition: all .3s;
+}
+/* 白色背景太单调了。来点背景 */
+html {
+  color: rgb(222,222,222);
+  background: rgb(0,43,54); 
+}
+/* 文字太近了 */
+.styleEditor {
+  padding: .5em;
+  border: 1px solid;
+  margin: .5em;
+  overflow: auto;
+  ${ isPc ? 'width: 48%;height: 96%;' : 'width: 96%;height: 50%;' }
+  font-size: 14px;
+  line-height:1.5;
+}
+/* 这些代码颜色都一样。加点儿高亮区别来 */
+.token.selector{ color: rgb(133,153,0) }
+.token.property{ color: rgb(187,137,0) }
+.token.punctuation{ color: yellow }
+.token.function{ color: rgb(42,161,152) }
+.token.comment{ color: rgb(177,177,177) }
+/* 加个 3D 效果 */
+html{
+  -webkit-perspective: 1000px;
+          perspective: 1000px;
+}
+.styleEditor {
+  position: fixed; 
+  ${ isPc ? 'left: 0;' : 'left:0;right:0;margin:auto;'}
+  top: 0; 
+  -webkit-transition: none; 
+  transition: none;   
+  ${ isPc ? '-webkit-transform: rotateY(10deg) translateZ(-100px) ;transform: rotateY(10deg) translateZ(-100px) ;' : '-webkit-transform: rotateX(-10deg) translateZ(-100px) ;transform: rotateX(-10deg) translateZ(-100px) ;' }
+  ${ isPc ? '' : '-webkit-transform-origin: 50% 0% 0;transform-origin: 50% 0% 0;' }
+}
+
+/* 再来一张记录我们这一年的纸张。他的名字叫resumeEditor */
+.resumeEditor{
+  position: fixed; 
+  ${ isPc ? 'right: 0;' : 'left:0;right:0;margin:auto;'}
+  ${ isPc ? 'top: 0;' : 'bottom:2%;'}
+  padding: .5em;  
+  ${ isPc ? 'margin: .5em;' : ''}
+  ${ isPc ? 'width: 48%;height: 96%;' : 'width: 96%;height: 50%;' }
+  border: 1px solid;
+  color: #222;
+  overflow: auto;
+  font-size: 14px;
+  line-height:1.5;
+  ${ isPc ? '-webkit-transform: rotateY(-10deg) translateZ(-100px) ;transform: rotateY(-10deg) translateZ(-100px) ;' : '-webkit-transform: rotateX(10deg) translateZ(-100px) ;transform: rotateX(10deg) translateZ(-100px) ;' }
+    ${ isPc ? '' : '-webkit-transform-origin: 50% 0% 0;transform-origin: 50% 0% 0;' }
+  }
+/* nice 准备工作完成！嘿嘿 */
+
+
+`,
+                `
+/* 是不是看着很简陋粗糙？
+ * 因为这是 Markdown 格式的
+ * 这是文档语音
+ * 翻译成 网页 html 就行了
+ */
+`,
+                `
+/* 再加点样式 */
+.resumeEditor{
+  padding: 2em;
+  line-height:1.8;
+}
+.resumeEditor h2{
+  display: inline-block;
+  border-bottom: 1px solid;
+  margin: 1em 0 .5em;
+  font-size:18px;
+}
+.resumeEditor ul,.resumeEditor ol{
+  list-style: none;
+}
+.resumeEditor ul> li::before{
+  content: '•';
+  margin-right: .5em;
+}
+.resumeEditor ol {
+  counter-reset: section;
+}
+.resumeEditor ol li::before {
+  counter-increment: section;            
+  content: counters(section, ".") " ";  
+  margin-right: .5em;
+}
+.resumeEditor blockquote {
+  margin: 1em;
+  padding: .5em;
+  background: rgba(221,221,221,.5);
+}
+
+/* OK。完成！ */
+
+`
+            ],
+            currentMarkdown: '',
+            fullMarkdown: `ZJK × LXH
+----
+
+2020年03月xx日。一句绑奶dd， 我们俩走在了一起 
+已有 \`${getDateDiff((new Date()).getFullYear()+'-'+((new Date()).getMonth()+1)+'-'+(new Date()).getDate(),'2020-04-20') + 1}\` 天
+
+一起去过的酒店
+----
+
+* cdcAI电竞
+* 朝沐精选
+* 魔杰骑士
+* 橙舍电竞
+* 熙牛
+* 卡希朵
+
+一起玩过的游戏
+----
+
+1. 守望先锋
+2. 我的世界
+3. 王者荣耀
+4. csgo
+5. 一梦江湖
+6. 奥雅之光
+7. ……
+
+一起看过的动漫
+----
+
+1. 血小板
+2. 吸血鬼
+3. 咒术
+4. 鬼灭
+5. ……
+
+> 虽然，这一年来我们的感情也不是一帆风顺。我们有过吵闹，有过悲伤。
+> 但是，我们还是坚持快一年了呢！！！！！
+> 宝贝，去年的你真的很棒。非常棒。
+> 你不比别人差。
+> 辛苦啦 去年的李欣航
+> 今年也要加油哦！！！
+
+> 开始放洋屁：
+> Dear XinHang:
+> If I could save time in a bottle
+> the first thing that I'd like to do is to save every day until eternity passes away
+> just to spend them with you
+> if I could make days last forever
+> if words could make wishes come true
+> I'd save every day like a treasure and then
+
+>
+>
+> 最后的最后 
+> 宝贝情人节快乐 新的一年也要加油哦 i老虎u！！
+
+`
+        }
+    },
+    created() {
+        this.makeResume()
+    },
+
+    methods: {
+        makeResume: async function() {
+            await this.progressivelyShowStyle(0)
+            await this.progressivelyShowResume()
+            await this.progressivelyShowStyle(1)
+            await this.showHtml()
+            await this.progressivelyShowStyle(2)
+        },
+        showHtml: function() {
+            return new Promise((resolve, reject) => {
+                this.enableHtml = true
+                resolve()
+            })
+        },
+        progressivelyShowStyle(n) {
+            return new Promise((resolve, reject) => {
+                let interval = this.interval
+                let showStyle = (async function() {
+                    let style = this.fullStyle[n]
+                    if (!style) {
+                        return
+                    }
+                    // 计算前 n 个 style 的字符总数
+                    let length = this.fullStyle.filter((_, index) => index <= n).map((item) => item.length).reduce((p, c) => p + c, 0)
+                    let prefixLength = length - style.length
+                    if (this.currentStyle.length < length) {
+                        let l = this.currentStyle.length - prefixLength
+                        let char = style.substring(l, l + 1) || ' '
+                        this.currentStyle += char
+                        if (style.substring(l - 1, l) === '\n' && this.$refs.styleEditor) {
+                            this.$nextTick(() => {
+                                this.$refs.styleEditor.goBottom()
+                            })
+                        }
+                        setTimeout(showStyle, interval)
+                    } else {
+                        resolve()
+                    }
+                }).bind(this)
+                showStyle()
+            })
+        },
+        progressivelyShowResume() {
+            return new Promise((resolve, reject) => {
+                let length = this.fullMarkdown.length
+                let interval = this.interval
+                let showResume = () => {
+                    if (this.currentMarkdown.length < length) {
+                        this.currentMarkdown = this.fullMarkdown.substring(0, this.currentMarkdown.length + 1)
+                        let lastChar = this.currentMarkdown[this.currentMarkdown.length - 1]
+                        let prevChar = this.currentMarkdown[this.currentMarkdown.length - 2]
+                        if (prevChar === '\n' && this.$refs.resumeEditor) {
+                            this.$nextTick(() => this.$refs.resumeEditor.goBottom())
+                        }
+                        setTimeout(showResume, interval)
+                    } else {
+                        resolve()
+                    }
+                }
+                showResume()
+            })
+        }
+    }
+}
+</script>
+<style scoped>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+html {
+    min-height: 100%;
+}
+.styleEditor {
+    -webkit-backface-visibility: hidden;
+}
+</style>
